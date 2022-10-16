4fa88ce99e681a20f7cc77844e1dcb9c8b33e0f2
knoffelcut <hardus_richter@hotmail.com>
2022-10-16 10:25:46 +0200

    Implement overlap to negate border effects

--- clog.py
+++ clog.py
@@ -4,12 +4,10 @@ import pathlib
 import numpy as np
 import skimage.io  # Preferred over opencv, since it is smaller and we are just doing io (perf not important)
 
-# cog predict r8.im/longguangwang/arbsr@sha256:9e20d2768e62c16716c585a899105a63cd5d36f6be17a208dbc201027325d881   -i image=@/home/hrichter/Pictures/BingWallpapers/20221015.jpg   -i target_width=2560   -i target_height=1440
-
 
 def upscale(path_input: str, target_width: int, target_height: int, directory_working: str = '/tmp'):
     assert pathlib.Path(path_input).exists()
-    path_output = (pathlib.Path(directory_working) / 'output.ext').with_suffix(pathlib.Path(path_input).suffix)
+    path_output = pathlib.Path(directory_working) / 'output.png'
     path_output.unlink(missing_ok=True)
     call = [
         'cog', 'predict',
@@ -23,14 +21,19 @@ def upscale(path_input: str, target_width: int, target_height: int, directory_wo
     return path_output
 
 
-def upscale_paths(path_input: str, target_width: int, target_height: int, parts_x: int = 2, parts_y: int = 2, directory_working: str = '/tmp'):
-    # TODO Overlap to negate the iffy side effects
+def upscale_parts(
+    path_input: str, target_width: int, target_height: int,
+    parts_x: int = 2, parts_y: int = 2, overlap: int = 128,
+    directory_working: str = '/tmp'
+):
     assert pathlib.Path(path_input).exists()
 
     path_input = pathlib.Path(path_input)
     image = skimage.io.imread(path_input)
     image_height = image.shape[0]
     image_width = image.shape[1]
+    fx = (target_width/image_width)
+    fy = (target_height/image_height)
 
     parts_x = list(np.linspace(0, image_width, parts_x, endpoint=False, dtype=int))
     parts_y = list(np.linspace(0, image_height, parts_y, endpoint=False, dtype=int))
@@ -40,26 +43,32 @@ def upscale_paths(path_input: str, target_width: int, target_height: int, parts_
     for y0, y1 in zip(parts_y, parts_y[1:] + [None, ]):
         parts_ = []
         for x0, x1 in zip(parts_x, parts_x[1:] + [None, ]):
-            part = image[y0:y1, x0:x1]
+            overlap_x = overlap/fx
+            overlap_y = overlap/fy
+            assert overlap_x == int(overlap_x) and overlap_y == int(overlap_y)
+            overlap_x, overlap_y = int(overlap_x), int(overlap_y)
+
+            py0, py1 = overlap_x*bool(y0), overlap_x*bool(y1)
+            px0, px1 = overlap_y*bool(x0), overlap_y*bool(x1)
+
+            part = image[y0 - py0:(y1 + py1) if y1 is not None else y1, x0 - px0:(x1 + px1) if x1 is not None else x1]
             path_part = (directory_working / 'input').with_suffix(path_input.suffix)
             skimage.io.imsave(path_part, part)
 
-            part_target_width = (target_width/image_width)*part.shape[1]
-            part_target_height = (target_height/image_height)*part.shape[0]
+            part_target_width = fx*part.shape[1]
+            part_target_height = fy*part.shape[0]
             assert part_target_width == int(part_target_width) and part_target_height == int(part_target_height)
             part_target_width, part_target_height = int(part_target_width), int(part_target_height)
             path_upscale = upscale(path_part, part_target_width, part_target_height, directory_working)
             part_upscale = skimage.io.imread(path_upscale)
+
+            # Remove the upscaled overlap sections
+            py0, py1 = int(fy*py0), int(fy*py1)
+            px0, px1 = int(fx*px0), int(fx*px1)
+            part_upscale = part_upscale[py0:-py1 if py1 > 0 else None, px0:-px1 if px1 > 0 else None]
+
             parts_.append(part_upscale)
         parts.append(parts_)
 
     upscaled = np.vstack([np.hstack(parts_) for parts_ in parts])
     return upscaled
-
-
-if __name__ == '__main__':
-    working_directory = '/tmp'
-    path_image = '/home/hrichter/Pictures/cog/output_512x512.png'
-
-    upscaled = upscale_paths(path_image, 1024, 1024)
-    skimage.io.imsave(pathlib.Path(working_directory) / 'output.png', upscaled)
